957782bf507745fad999db39f307db3c125b056a
Nikolay_Liemiekhov <nicklemster@gmail.com>
2023-03-08 19:15:20 +0200

    feat: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,11 @@ module.exports = {
     ecmaVersion: 12,
   },
   rules: {
+    'react/jsx-indent' : ['error', 2],
+    'react/jsx-indent-props': ['error', 2],
+    'indent': ['error', 2, { SwitchCase: 1 }],
     semi: [2, 'always'],
     'comma-dangle': ['error', 'always-multiline'],
+    'quotes': ['error', 'single', { avoidEscape: true }],
   },
 };
