92f034baebbaba58b67189213a5f62c12d16d16f
Nikolay_Liemiekhov <nicklemster@gmail.com>
2023-03-09 12:11:43 +0200

    feat: add not found page navigation

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,15 @@
+import {Stack, Typography} from '@mui/material';
+import {NavLink} from 'react-router-dom';
+
 export default function NotFoundPage() {
   return (
-    <div>NotFoundPage</div>
+    <Stack gap={4}>
+      <Typography variant="h2" component="h1" textAlign='center'>
+        404 Page not found.
+      </Typography>
+      <Typography variant="h5" component="p" textAlign='center'>
+        Оберіть сторінку в меню, або перейдіть на <NavLink to='/'>домашню сторінку</NavLink>.
+      </Typography>
+    </Stack>
   );
 }
